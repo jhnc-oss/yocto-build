116e440aa4394370d9e2e3a654a5a47e52e2e12c
h5b <h5b@users.noreply.github.com>
2022-07-21 12:10:30 +0200

    Update to latest LTS release (dunfell-23.0.17)

--- dev/init_env.sh
+++ dev/init_env.sh
@@ -2,7 +2,7 @@
 
 set -o errexit
 
-MANIFEST_BRANCH="dunfell-23.0.15"
+MANIFEST_BRANCH="dunfell-23.0.17"
 MANIFEST_URL="https://github.com/jhnc-oss/yocto-manifests.git"
 PATH="$HOME/.local/bin:$PATH"
 
