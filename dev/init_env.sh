2c75e821155a1b5b27ff1e7cfe14ed701a7903b0
offa <bm-dev@yandex.com>
2025-01-31 09:45:42 +0100

    dev

--- dev/init_env.sh
+++ dev/init_env.sh
@@ -2,21 +2,18 @@
 
 set -o errexit
 
-MANIFEST_BRANCH="${1:-scarthgap}"
+MANIFEST_BRANCH="${1:-main}"
 MANIFEST_URL="https://github.com/jhnc-oss/yocto-manifests.git"
 
 repo init \
-  --manifest-url $MANIFEST_URL \
+  --manifest-url ${MANIFEST_URL} \
   --no-clone-bundle \
   --depth=1 \
-  --manifest-branch $MANIFEST_BRANCH
+  --manifest-branch ${MANIFEST_BRANCH}
 
 repo sync \
-  --jobs="$(grep -c ^proc /proc/cpuinfo)" \
   --fetch-submodules \
   --current-branch \
   --no-clone-bundle
 
 source poky/oe-init-build-env
-
-exec /bin/bash
